dd6b560a2a69b72a2a90725bd5295d81fb237464
Christian Falch <christian.falch@gmail.com>
2015-10-12 13:56:00 +0200

    Updated with binding extensions.

---
+++ Template/Test.NewSolution.FormsApp/Extensions/BindingExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Test.NewSolution.FormsApp.Extensions
+{
+    /// <summary>
+    /// Binding extensions.
+    /// </summary>
+    public static class BindingExtensions
+    {
+        /// <summary>
+        /// Sets up a binding
+        /// </summary>
+        /// <param name="bindableObject">Bindable object.</param>
+        /// <param name="bindableProperty">Bindable property.</param>
+        /// <param name="viewModelProperty">View model property.</param>
+        public static View BindTo(this View view, BindableProperty bindableProperty, string propertyName){
+
+            view.SetBinding(bindableProperty, propertyName);
+            return view;
+        }
+    }
+}
+
